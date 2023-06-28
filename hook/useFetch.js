73933969e878b7bab1eb6b5d729cfb563169a6c4
SAIKmar-1729 <83260137+SAIKmar-1729@users.noreply.github.com>
2023-06-29 03:57:02 +0530

    It is deployed or published in Expo.The first react native App done for learning and Completed in 9 hours

---
+++ hook/useFetch.js
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+// import { RAPID_API_KEY } from '@env';
+
+// const rapidApiKey = RAPID_API_KEY;
+
+const useFetch = (endpoint, query) => {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const options = {
+        method: 'GET',
+        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+        headers: {
+            'X-RapidAPI-Key': '155a34d627msh196dc2a69785d4bp15cf8ejsn919efdadf7dc',
+            'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+        },
+        params: { ...query },
+
+    };
+
+    const fetchData = async () => {
+        setIsLoading(true);
+        try {
+            const response = await axios.request(options);
+            setData(response.data.data);
+            setIsLoading(false);
+        } catch (error) {
+            setError(error);
+            alert('There is an error')
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const refetch = () => {
+        setIsLoading(true);
+        fetchData();
+    }
+
+    return { data, isLoading, error, refetch }
+}
+
+export default useFetch;
+// '492e095180msh75479ab062fabcfp139909jsn8f7c478a2393' ->r.saikumar
